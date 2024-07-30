e3cbf3e0ae12eb6ed22105f5ef30121f46a0af77
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-29 23:10:42 -0300

    Add new colors and plugin in the file tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'main-blue-50': 'var(--main-blue-50)',
+        'main-blue-100': 'var(--main-blue-100)',
+        'main-blue-200': 'var(--main-blue-200)',
+        'main-blue-300': 'var(--main-blue-300)',
+        'main-blue-400': 'var(--main-blue-400)',
+        'main-blue-500': 'var(--main-blue-500)',
+        'main-blue-600': 'var(--main-blue-600)',
+        'main-blue-700': 'var(--main-blue-700)',
+        'main-blue-800': 'var(--main-blue-800)',
+        'main-blue-900': 'var(--main-blue-900)',
+        'main-blue-950': 'var(--main-blue-950)',
+        'blue-text': 'rgb(140, 219, 229)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
 export default config;
