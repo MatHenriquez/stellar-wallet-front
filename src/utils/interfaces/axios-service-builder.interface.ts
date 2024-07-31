ef9a26a2fe5f9909a595ca3c126707dfcbc3eff2
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-07-30 23:34:39 -0300

    Update AxiosServiceBuilder

--- src/utils/interfaces/axios-service-builder.interface.ts
+++ src/utils/interfaces/axios-service-builder.interface.ts
@@ -4,6 +4,5 @@ import AxiosServiceBuilder from '../axios-service-builder';
 export interface IAxiosServiceBuilder {
   WithBaseUrl(baseUrl: string): AxiosServiceBuilder;
   WithTimeout(timeout: number): AxiosServiceBuilder;
-  WithAccesToken(accessToken: string | null): AxiosServiceBuilder;
   Build(): AxiosInstance;
 }
