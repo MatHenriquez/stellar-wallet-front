06de89b242d306f8876239255101511d2f089018
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-02 20:44:53 -0300

    Add Cypress tests for the login page

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,73 @@
+describe('Signup page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/auth/login');
+  });
+
+  describe('Form', () => {
+    it('should display the form', () => {
+      cy.get('[data-cy=login-form]').should('exist');
+    });
+
+    it('should display the title', () => {
+      cy.get('[data-cy=title]').should('exist');
+    });
+
+    it('should display the email input', () => {
+      cy.get('[data-cy=email-input]').should('exist');
+    });
+
+    it('should display the password input', () => {
+      cy.get('[data-cy=password-input]').should('exist');
+    });
+
+    it('should display the submit button', () => {
+      cy.get('[data-cy=submit-button]').should('exist');
+    });
+  });
+
+  describe('Form validation', () => {
+    it('should display an error message when email is invalid', () => {
+      cy.get('[data-cy=email-input]').type('invalid-email');
+      cy.get('[data-cy=submit-button]').click();
+      cy.get('[data-cy=email-error]').should('exist').should('have.text', '*Invalid email');
+    });
+
+    it('should display an error message when email is empty', () => {
+      cy.get('[data-cy=submit-button]').click();
+      cy.get('[data-cy=email-error]').should('exist').should('have.text', '*Required');
+    });
+
+    it('should display an error message when password is empty', () => {
+      cy.get('[data-cy=submit-button]').click();
+      cy.get('[data-cy=password-error]').should('exist').should('have.text', '*Required');
+    });
+  });
+
+  describe('Form submission', () => {
+    it('should redirect to home page when form is submitted', () => {
+      cy.intercept('POST', '/Auth/Login', {
+        statusCode: 200,
+        body: {
+          success: true,
+          token: 'token',
+          publicKey: 'publicKey',
+        },
+      }).as('successfulLogin');
+      cy.get('[data-cy=email-input]').type('valid@email.com');
+      cy.get('[data-cy=password-input]').type('password');
+      cy.get('[data-cy=submit-button]').click();
+      cy.wait('@successfulLogin');
+      cy.url().should('eq', 'http://localhost:3000/home');
+    });
+
+    it('should not redirect to home page when form is submitted with invalid credentials', () => {
+      cy.intercept('POST', '/Auth/Login', {
+        statusCode: 400,
+      }).as('failedLogin');
+      cy.get('[data-cy=email-input]').type('invalid@email.com');
+      cy.get('[data-cy=password-input]').type('password');
+      cy.get('[data-cy=submit-button]').click();
+      cy.url().should('eq', 'http://localhost:3000/auth/login');
+    });
+  });
+});
