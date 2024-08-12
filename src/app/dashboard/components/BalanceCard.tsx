4eb45ba8b85a71a88bf8b4d6328eb36168c188ad
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-12 20:45:44 -0300

    Add BalanceCard component and styles

---
+++ src/app/dashboard/components/BalanceCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IBalance } from '../interfaces/balances-response.interface';
+import styles from '../styles/BalanceCard.module.css';
+
+const BalanceCard = ({ asset, amount }: IBalance) => {
+  return (
+    <div className={styles.container} data-cy={`balance-card-${asset}`}>
+      <div className={styles.cardBody}>
+        <h2 className={styles.cardTitle}>Asset: {asset == 'native' ? 'XLM' : asset}</h2>
+        <p>Amount: {amount}</p>
+        <div className={styles.cardAction}>
+          <button className={styles.cardButton}>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BalanceCard;
