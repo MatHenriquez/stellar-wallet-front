7f162e7353cf7e8af641338a2d6eb9ffa54d0de1
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-13 00:28:49 -0300

    Add default command timeout for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: process.env.NEXT_PUBLIC_E2E_BASE_URL ?? 'http://localhost:3000',
+    defaultCommandTimeout: 10000,
   },
 
   component: {
