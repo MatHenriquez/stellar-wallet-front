7a9e8421835d783efac424ea673d2ac68acad3aa
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-13 09:02:44 -0300

    Add default command timeout and retries for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: process.env.NEXT_PUBLIC_E2E_BASE_URL ?? 'http://localhost:3000',
+    defaultCommandTimeout: 40000,
+    retries: 2,
   },
 
   component: {
