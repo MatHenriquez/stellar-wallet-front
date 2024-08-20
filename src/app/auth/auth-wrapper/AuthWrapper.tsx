8002b82481b0239b9cc178b6692e17858e1ba130
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-19 23:06:09 -0300

    Add AuthWrapper component

---
+++ src/app/auth/auth-wrapper/AuthWrapper.tsx
@@ -0,0 +1,43 @@
+'use client';
+import axiosInstance from '@/services/axios-instance';
+import { useRouter } from 'next/navigation';
+import React, { ReactNode, useEffect } from 'react';
+
+interface AuthWrapperProps {
+  children: ReactNode;
+}
+
+const AuthWrapper = ({ children }: AuthWrapperProps) => {
+  const router = useRouter();
+
+  const handleRedirect = (currentUrl: string, isAuthenticated: boolean) => {
+    const shouldRedirectToDashboard = currentUrl === '/auth/login' || currentUrl === '/auth/signup' || currentUrl === '/';
+
+    if (shouldRedirectToDashboard) {
+      if (isAuthenticated) {
+        router.push('/dashboard');
+      } else {
+        router.push('/auth/login');
+      }
+    } else if (!isAuthenticated) {
+      router.push('/auth/login');
+    }
+  };
+
+  useEffect(() => {
+    axiosInstance
+      .get('/Auth/UserToken')
+      .then((response) => {
+        if (response.data) {
+          handleRedirect(window.location.pathname, true);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        handleRedirect(window.location.pathname, false);
+      });
+  }, []);
+  return <>{children}</>;
+};
+
+export default AuthWrapper;
